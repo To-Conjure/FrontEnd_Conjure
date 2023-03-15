fa67a20381d3cc097dd37008e23f25cdc1c60c3f
evanlu1134 <evanlu1134@gmail.com>
2023-03-15 01:32:14 -0400

    Updates

--- src/components/menuPath/Error.js
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-    return (
-        <>
-            <h1>Error Path</h1>
-        </>
-    )
-}
-
-export default Error
--- src/components/menuPath/Learn.js
+++
@@ -1,9 +0,0 @@
-const Learn = () => {
-    return (
-        <div>
-            <p>learn more</p>
-        </div>
-    )    
-}
-
-export default Learn
